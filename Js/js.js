94ba2db3a983f31c5428b3a67f876f8bf781ab28
matias-cavalcante <matias.cavalcante93@gmail.com>
2021-09-08 22:12:23 +0000

    Refactor whole code. Not affect broadly desing.

--- Js/js.js
+++ Js/js.js
@@ -1,24 +1,8 @@
 
-/*
-function checkFluency()
-{
-  var checkbox = document.getElementById("nav-check");
-  var menu = document.getElementById("menu-list");
-  if ( window.matchMedia("(max-width: 700px)") && checkbox.checked == true)
-  {
-    menu.style.display = "block";
-  }
-  else if ( window.matchMedia("(max-width: 700px)") && checkbox.checked == false){
-    menu.style.display = "none";
-  }
-}*/
 
-const hamburguer = document.getElementsById("hamburguer");
-const navUL = document.getElementsById("menu-list");
-
-hamburguer.addEventListener('click', () =>){
-  navUL.classList.toggle("show");
+var el = document.getElementById('home')
+var clickerFn = function () {
+  console.log('Click just happened')
 }
 
-
-
+el.onclick = clickerFn
